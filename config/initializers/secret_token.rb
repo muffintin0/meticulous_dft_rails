397862ec32abd64f00b107d1d324678db4f64504
muffintin0 <hmchem@gmail.com>
2013-04-30 13:02:22 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeticulousDft::Application.config.secret_token = 'd1f669fd19da7045bd986d78d96270f522eb605a2e2084bab3fccc7b3a8d6d8abef6efd7da6172cca5808283e470c68575ca0ec3e7ead93ea383dc8994b1bb47'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MeticulousDft::Application.config.session_store :cookie_store, key: '_Meticulous_DFT_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MeticulousDft::Application.config.session_store :active_record_store
